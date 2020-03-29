be5d3eb693fb9d6eb68992705952be0e1f1ea41c
Yves Gabriel <Yves.Gabriel@equisoft.com>
2020-03-29 00:59:46 -0400

    added missing requirement

--- db.js
+++ db.js
@@ -3,7 +3,6 @@ const mongoose = require("mongoose");
 const appConfig = require("./appConfig");
 const dbConnectionUrl = appConfig.db.dbConnectionUrl;
 
-console.log(dbConnectionUrl);
 const connectDb = async () => {
   await mongoose
     .connect(dbConnectionUrl, {
--- server.js
+++ server.js
@@ -1,15 +1,14 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-const connectDb = require("./db");
 const avalonRouter = require("./api/routes/avalonRoutes");
-
+const path = require("path");
 const app = express();
 const port = process.env.PORT || 5000;
-
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+const connectDb = require("./db");
 
 connectDb();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use("/api/avalon", avalonRouter);
 
 if (process.env.NODE_ENV === "production") {
