e28ee6a38226925dd23a46b205c06a925cf8503e
Yves Gabriel <Yves.Gabriel@equisoft.com>
2020-03-29 18:39:09 -0400

    changed images

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
-import blueSrc from "./images/lancelot.jpg";
-import redSrc from "./images/mordred.jpg";
+import blueSrc from "./images/lancelot.png";
+import redSrc from "./images/mordred.png";
 import "./App.css";
 import AvalonForm from "./comoponents/AvalonForm";
 import AvalonResult from "./comoponents/AvalonResults";
